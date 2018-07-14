76771aaef938a173847561133b79d0d5ac374871
Rajvir Rai <rsr1992@gmail.com>
2018-07-14 15:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase)
+  if (string.toLowerCase() === string)
   return "I can't hear you!"
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === uppercase)
+  if (string.toUpperCase() === string)
   return "YES INDEED!"
 }
