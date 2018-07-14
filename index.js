290a9e4520df3210f0ed73aeb050459187de20e2
Rajvir Rai <rsr1992@gmail.com>
2018-07-14 15:46:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
   return "I can\'t hear you!"
 }
-  if (string.toUpperCase() === string)
+if (string.toUpperCase() === string)
   return "YES INDEED!"
 }
 
